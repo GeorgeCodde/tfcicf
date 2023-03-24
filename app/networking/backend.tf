af47bbaff014bec827e0d7c86c56151cfe26d646
Jorge López <jorge.lopez@sistemascasa.mx>
2023-03-24 13:30:43 -0600

    Add backend

--- app/networking/backend.tf
+++ app/networking/backend.tf
@@ -1,11 +1,11 @@
 ## Move this backend file to m3 when migrating state
 terraform {
   backend "s3" {
-    bucket = "tf-backend-xxxxxx"
+    bucket = "tf-backend-16170"
     key    = "networking/terraform.tfstate"
     region = "eu-central-1"
 
-    dynamodb_table = "tf-backend-locks-xxxxx"
+    dynamodb_table = "tf-backend-locks-16170"
     encrypt        = true
   }
 }
